f86af18ea96a2afec10cc7ba88bd6c5ab25f0a82
Matheus Ribeiro <matthew.infwork@gmail.com>
2024-09-18 23:46:54 -0300

    Criando divisão para são paulo e niteroi

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 //Pages
-import Home from './Pages/Home';
 import Fares from './Pages/Fares';
 import Loyalty from './Pages/Loyalty';
 import Guide from './Pages/Guide';
@@ -11,12 +10,16 @@ import ReducedFare from './Pages/ReducedFare';
 import Concept from './Pages/Concept';
 import TrabalheConosco from './Pages/WorkWithUs';
 import FormContact from './Pages/Contact';
+import SaoPaulo from './components/City/SaoPaulo/SaoPaulo';
+import Niteroi from './components/City/Niteroi/Niteroi';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<SaoPaulo/>} />
+        <Route path="/Saopaulo" element={<SaoPaulo/>} />
+        <Route path="/Niteroi" element={<Niteroi/>} />
         <Route path="/Fares" element={<Fares />} />
         <Route path="/Guide" element={<Guide />} />
         <Route path="/Loyalty" element={<Loyalty />} />
---
+++ src/components/City/Niteroi/Niteroi.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from '../../navbar/navbar'
+import Slider from '../../Slider/Slider';
+import NowPlaying from "../../PlayingNow/PlayingNow";
+import News from "../../News/News";
+import Footer from "../../Footer/Footer";
+
+function Niteroi () {
+    return (
+        <section>
+            <Navbar />
+            <Slider />
+            <NowPlaying />
+            <News />
+            <Footer/>
+        </section>
+    );
+};
+
+export default Niteroi;
