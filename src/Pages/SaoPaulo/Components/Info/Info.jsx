f4be1f2a9596e5f2dd6fdf00d19b0fd2b56d7c45
Matheus Ribeiro <125816701+MatheusSlvRibeiro@users.noreply.github.com>
2024-09-25 19:58:50 -0300

    Corrigindo href's

--- src/Pages/SaoPaulo/Components/Info/Info.jsx
+++ src/Pages/SaoPaulo/Components/Info/Info.jsx
@@ -67,7 +67,7 @@ function Info() {
                     </div>
 
                     <div className={Styles.Img}>
-                        <a href="/GUIDE_SP">
+                        <a href="/guide_sp">
                             <img
                                 src="/images/Guide.jpg"
                                 alt="Banner Guia do Bom Espectador com ícones de cinema e botão 'Saiba Mais"
