0c8720c62d0223fb7daf3645a65f65c999404d1c
Matheus Ribeiro <matthew.infwork@gmail.com>
2024-09-26 23:01:01 -0300

    Criando SubMenu para Navbar/RJ-SP

--- src/Pages/Niteroi/Componentes/navbar/navbar.js
+++ src/Pages/Niteroi/Componentes/navbar/navbar.js
@@ -9,11 +9,41 @@ const NavBar = () => {
     const [dropdownOpen, setDropdownOpen] = useState(false);
 
     const navItems = [
-        { id: 1, text: 'Programação', url: '/Niteroi/#em_cartaz' },
-        { id: 2, text: 'Gastronomia', url: 'https://linktr.ee/reservaculturalsp?fbclid=PAZXh0bgNhZW0CMTEAAabqWzJl6zTG3UtiPRTWnC8zr3pWonCUuq226x34JeR0tkpZf8ekFUDFmvU_aem_Zlg-S94_9wl7df3gnCguhQ' },
-        { id: 3, text: 'Eventos', url: '/eventos' },
-        { id: 4, text: 'Fidelidade', url: '/Fidelidade' },
-        { id: 5, text: 'Parceria', url: '/parceria' },
+        { 
+            id: 1, text: 'Programação', url: '#', 
+            subItems: [
+                { text: 'Em Cartaz', url: '/Niteroi/#em_cartaz' },
+                { text: 'Ingressos', url: 'https://www.ingresso.com/cinema/cinema-reserva-cultural-niteroi?city=niteroi' },
+                { text: 'Tarifas', url: '/Tarifas' },
+                { text: 'Meia-entrada', url: '/MeiaEntrada' },
+                { text: 'Guia do Bom Espectador', url: '/Guia' }
+            ]
+        },
+        { 
+            id: 2, text: 'Gastronomia', url: '#', 
+            subItems: [
+                { text: 'Mahatma Pizza', url: 'https://www.instagram.com/mahatmapizza/' },
+                { text: 'Brasserie Reserva', url: 'https://www.instagram.com/brasseriereserva/' },
+                { text: 'Dropburger', url: 'https://www.instagram.com/dropburger/' }
+            ]
+        },
+        { 
+            id: 3, text: 'Eventos', url: '#', 
+            subItems: [
+                { text: 'Eventos Realizados', url: '#realizados' },
+                { text: 'Parcerias', url: '#parcerias' }
+            ]
+        },
+        { 
+            id: 4, text: 'O Reserva', url: '/Conceito', 
+            subItems: [
+                { text: 'O Conceito', url: '/Conceito' },
+                { text: 'Fidelidade', url: '/Fidelidade' },
+                { text: 'Estacionamento', url: 'https://www.google.com/maps/place/Av.+Visconde+do+Rio+Branco,+880+-+S%C3%A3o+Domingos,+Niter%C3%B3i+-+RJ,+24210-200/@-22.8982811,-43.1332801,17z/data=!3m1!4b1!4m6!3m5!1s0x9983e476739ca3:0x100a0ab61b58ef51!8m2!3d-22.8982861!4d-43.1307052!16s%2Fg%2F11pvbz_pzn?entry=ttu&g_ep=EgoyMDI0MDkyMi4wIKXMDSoASAFQAw%3D%3D' },
+                { text: 'Trabalhe Conosco', url: 'TrabalheConosco' },
+                { text: 'Contato', url: '/Contato' }
+            ]
+        }
     ];
 
     const renderNavItems = () => {
@@ -22,17 +52,23 @@ const NavBar = () => {
                 <a className={styles.navLink} href={item.url}>
                     {item.text}
                 </a>
+                {item.subItems && (
+                    <ul className={styles.subMenu}>
+                        {item.subItems.map((subItem, index) => (
+                            <li key={index}>
+                                <a className={styles.subNavLink} href={subItem.url}>
+                                    {subItem.text}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                )}
             </li>
         ));
     };
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
-    };
-
-    // Funções do antigo componente Location
-    const handleToggleDropdown = () => {
-        setDropdownOpen(!dropdownOpen);
     };
 
     return (
@@ -47,7 +83,7 @@ const NavBar = () => {
 
             {/* Código do Location diretamente aqui */}
             <div className={styles.LocationContainer}>
-                <div className={styles.Location} onClick={handleToggleDropdown}>
+                <div className={styles.Location} onClick={() => setDropdownOpen(!dropdownOpen)}>
                     <img className={styles.Icon} src="/images/location.png" alt="Ícone de localização" />
                     <h4 className={styles.State}>Niterói</h4>
                     <img className={styles.Arrow} src="/images/downArrow.png" alt="Seta para baixo" />
@@ -55,14 +91,13 @@ const NavBar = () => {
 
                 {dropdownOpen && (
                     <div className={styles.Dropdown}>
-                        <div  className={styles.LocationItem}>
+                        <div className={styles.LocationItem}>
                             <a href="/SãoPaulo" className={styles.LocationItem}>
                                 <h4>São Paulo - SP</h4>
                                 <p>Avenida Paulista, 900 - Térreo Baixo</p>
                             </a>
-                        </ div>
-
-                        <div  className={styles.LocationItem}>
+                        </div>
+                        <div className={styles.LocationItem}>
                             <a href="/niteroi">
                                 <h4>Niterói - RJ</h4>
                                 <p>Avenida Visconde do Rio Branco 880</p>
--- src/Pages/SaoPaulo/Components/navbar/navbar.js
+++ src/Pages/SaoPaulo/Components/navbar/navbar.js
@@ -9,11 +9,40 @@ const NavBar = () => {
     const [dropdownOpen, setDropdownOpen] = useState(false);
 
     const navItems = [
-        { id: 1, text: 'Programação', url: '/SãoPaulo/#em_cartaz' },
-        { id: 2, text: 'Gastronomia', url: 'https://linktr.ee/reservaculturalsp?fbclid=PAZXh0bgNhZW0CMTEAAabqWzJl6zTG3UtiPRTWnC8zr3pWonCUuq226x34JeR0tkpZf8ekFUDFmvU_aem_Zlg-S94_9wl7df3gnCguhQ' },
-        { id: 3, text: 'Eventos', url: '/eventos' },
-        { id: 4, text: 'Fidelidade', url: '/FidelidadeSp' },
-        { id: 5, text: 'Parceria', url: '/parceria' },
+        { 
+            id: 1, text: 'Programação', url: '#', 
+            subItems: [
+                { text: 'Em Cartaz', url: '/SãoPaulo/#em_cartaz' },
+                { text: 'Ingressos', url: 'https://www.ingresso.com/cinema/cinema-reserva-cultural-sao-paulo?city=sao-paulo' },
+                { text: 'Tarifas', url: '/TarifasSp' },
+                { text: 'Meia-entrada', url: '/MeiaEntradaSp' },
+                { text: 'Guia do Bom Espectador', url: '/GuiaSp' }
+            ]
+        },
+        { 
+            id: 2, text: 'Gastronomia', url: '#', 
+            subItems: [
+                { text: 'Buffet pain de france', url: 'https://drive.google.com/file/d/1r1duntiYfC_eTOw-I71YMWEsy2oh5DJM/view' },
+                { text: 'bistro reserva', url: 'https://api.whatsapp.com/send?phone=5511914339374' },
+            ]
+        },
+        { 
+            id: 3, text: 'Eventos', url: '#', 
+            subItems: [
+                { text: 'Eventos Realizados', url: '#realizados' },
+                { text: 'Parcerias', url: '#parcerias' }
+            ]
+        },
+        { 
+            id: 4, text: 'O Reserva', url: '/ConceitoSp', 
+            subItems: [
+                { text: 'O Conceito', url: '/ConceitoSp' },
+                { text: 'Fidelidade', url: '/FidelidadeSp' },
+                { text: 'Estacionamento', url: 'https://www.google.com/maps/place/R.+S%C3%A3o+Carlos+do+Pinhal,+303+-+Bela+Vista,+S%C3%A3o+Paulo+-+SP,+01333-001/@-23.56475,-46.6532284,17z/data=!3m1!4b1!4m6!3m5!1s0x94ce59b86d92b715:0xba496b2341d67afd!8m2!3d-23.5647549!4d-46.6506535!16s%2Fg%2F11c5lzbhzt?entry=tts&g_ep=EgoyMDI0MDgyOC4wKgBIAVAD' },
+                { text: 'Trabalhe Conosco', url: 'TrabalheConoscoSp' },
+                { text: 'Contato', url: '/ContatoSp' }
+            ]
+        }
     ];
 
     const renderNavItems = () => {
@@ -22,17 +51,23 @@ const NavBar = () => {
                 <a className={styles.navLink} href={item.url}>
                     {item.text}
                 </a>
+                {item.subItems && (
+                    <ul className={styles.subMenu}>
+                        {item.subItems.map((subItem, index) => (
+                            <li key={index}>
+                                <a className={styles.subNavLink} href={subItem.url}>
+                                    {subItem.text}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                )}
             </li>
         ));
     };
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
-    };
-
-    // Funções do antigo componente Location
-    const handleToggleDropdown = () => {
-        setDropdownOpen(!dropdownOpen);
     };
 
     return (
@@ -47,22 +82,21 @@ const NavBar = () => {
 
             {/* Código do Location diretamente aqui */}
             <div className={styles.LocationContainer}>
-                <div className={styles.Location} onClick={handleToggleDropdown}>
+                <div className={styles.Location} onClick={() => setDropdownOpen(!dropdownOpen)}>
                     <img className={styles.Icon} src="/images/location.png" alt="Ícone de localização" />
-                    <h4 className={styles.State}>São Paulo</h4>
+                    <h4 className={styles.State}>Niterói</h4>
                     <img className={styles.Arrow} src="/images/downArrow.png" alt="Seta para baixo" />
                 </div>
 
                 {dropdownOpen && (
                     <div className={styles.Dropdown}>
-                        <div  className={styles.LocationItem}>
+                        <div className={styles.LocationItem}>
                             <a href="/SãoPaulo" className={styles.LocationItem}>
                                 <h4>São Paulo - SP</h4>
                                 <p>Avenida Paulista, 900 - Térreo Baixo</p>
                             </a>
-                        </ div>
-
-                        <div  className={styles.LocationItem}>
+                        </div>
+                        <div className={styles.LocationItem}>
                             <a href="/niteroi">
                                 <h4>Niterói - RJ</h4>
                                 <p>Avenida Visconde do Rio Branco 880</p>
