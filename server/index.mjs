5f62d1a3fef4bbe3c46114acf34211d5bc305982
Matheus Ribeiro <125816701+MatheusSlvRibeiro@users.noreply.github.com>
2024-12-03 17:51:27 -0300

    :bug: fix: Corrigindo erro de requisição ao backend

--- server/index.mjs
+++ server/index.mjs
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 const cors = require('cors')
 
 app.use(cors({
-    origin: [  'http://localhost:3000', 'https://reservacultural.netlify.app' ]
+    origin: [  'http://localhost:3000', 'https://cinemareservacultural.netlify.app/' ]
 }));
 app.use(bodyParser.json());
 
