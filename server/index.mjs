6693d248c86e236eb26cce3e02af74bfcb03a3a7
Matheus Ribeiro <125816701+MatheusSlvRibeiro@users.noreply.github.com>
2024-12-03 18:27:49 -0300

    :bug: fix: Corrigindo erro de requisição ao backend

--- server/index.mjs
+++ server/index.mjs
@@ -11,7 +11,6 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors')
 
 app.use(cors({
     origin: [  'http://localhost:3000', 'https://cinemareservacultural.netlify.app/' ]
