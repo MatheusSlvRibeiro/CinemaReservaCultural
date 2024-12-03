1e777d685fab2fcae4d9ef08a936bb466ea59ad8
Matheus Ribeiro <125816701+MatheusSlvRibeiro@users.noreply.github.com>
2024-12-03 17:48:48 -0300

    :bug: fix: Corrigindo erro de requisição ao backend

--- server/index.mjs
+++ server/index.mjs
@@ -11,6 +11,7 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors')
 
 app.use(cors({
     origin: [  'http://localhost:3000', 'https://reservacultural.netlify.app' ]
